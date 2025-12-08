33cf04ae562d0344a042fa638ee7547027643f76
yoko-Developer <whitesnow.wave@gmail.com>
2025-12-08 16:58:29 +0900

    application repository実装

---
+++ src/test/java/com/example/airead_batch/AireadBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.airead_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AireadBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
